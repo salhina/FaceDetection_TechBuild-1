92acf9872e6b7a4cbd59dc3839c80b889bc82df7
Sachin Bhatia <sachinbhatia98@hotmail.com>
2016-08-24 19:18:31 +0400

    Added eye detection code and improved face detection

--- OpenCV_Run/Source.cpp
+++ OpenCV_Run/Source.cpp
@@ -11,11 +11,15 @@ using namespace cv;
 // Function Headers
 void detectAndDisplay(Mat frame);
 void CallBackFunc(int event, int x, int y, int flags, void* userdata);
+//void Rect_Point(Rect, Point);
 
 // Global variables
 // Copy this file from opencv/data/haarscascades to target folder
 string face_cascade_name = "haarcascade_frontalface_alt.xml";
 CascadeClassifier face_cascade;
+string eye_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
+CascadeClassifier eye_cascade;
+
 string window_name = "Capture - Face detection";
 
 
@@ -26,13 +30,13 @@ int main(void)
 
 	if (!capture.isOpened())  // check if we succeeded
 		return -1;
+     
+	capture.set(CAP_PROP_FPS, 1);
 
 	// Load the cascade
-	if (!face_cascade.load(face_cascade_name))
-	{
-		printf("--(!)Error loading\n");
-		return (-1);
-	};
+	if (!face_cascade.load(face_cascade_name)){ printf("--(!)Error loading\n"); return (-1); }
+
+	if (!eye_cascade.load(eye_cascade_name)){ printf("--(!)Error loading\n"); return (-1); }
 
 	// Read the video stream
 	Mat frame;
@@ -52,7 +56,6 @@ int main(void)
 		}
 
 		int c = waitKey(10);
-
 		if (27 == char(c))
 			break;
 	}
@@ -63,7 +66,8 @@ int main(void)
 // Function detectAndDisplay
 void detectAndDisplay(Mat frame)
 {
-	std::vector<Rect> faces;
+	std::vector<Rect> faces, eyes;
+	
 	Mat frame_gray, crop, res, gray;
 	string text;
 	stringstream sstm;
@@ -71,55 +75,101 @@ void detectAndDisplay(Mat frame)
 	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
 	equalizeHist(frame_gray, frame_gray);
 
-	// Detect faces
-	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
-
+	// Detect faces and eyes
+	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(40, 40));
+	
 	Point cen_win(frame.size().width / 2, frame.size().height / 2);
 	circle(frame, cen_win, 1, Scalar(0, 0, 255), CV_FILLED);
 	
+	Point pt[2];
+	Point cen_rec;
+
 	size_t ic = 0; // ic is index of current element
 	unsigned long ac = 0; // ac is area of current element
 
 	size_t ib = 0; // ib is index of biggest element
 	unsigned long ab = 0; // ab is area of biggest element
 	
-	for (ic = 0; ic < faces.size(); ic++) // Iterate through all current elements (detected faces)
+	for (int i = 0; i < faces.size(); i++) 
 	{
+		pt[0].x = faces[i].x;
+		pt[0].y = faces[i].y;
+
+		pt[1].x = faces[i].x + faces[i].width;
+		pt[1].y = faces[i].y + faces[i].height;
+
+		rectangle(frame, pt[0], pt[1], Scalar(255, 255, 0), 2, 8, 0);
+	
+		cen_rec.x = faces[i].x + faces[i].width / 2;
+		cen_rec.y = faces[i].y + faces[i].height / 2;
+		circle(frame, cen_rec, 1, Scalar(0, 0, 255), CV_FILLED); //Marks the center of detected face.
+		
+	}
+		
+	for (ic = 0; ic < faces.size(); ic++) // Iterate through all current elements (detected faces)
+	{	
 		ac = faces[ic].area();
 		ab = faces[ib].area();
 
 		if (ac > ab)
+		{
 			ib = ic; // no need for ic = 0 condition as ib is zero by default
+			
+			pt[0].x = faces[ib].x;
+			pt[0].y = faces[ib].y;
+
+			pt[1].x = faces[ib].x + faces[ib].width;
+			pt[1].y = faces[ib].y + faces[ib].height;
+		
+			cen_rec.x = faces[ib].x + faces[ib].width / 2;
+			cen_rec.y = faces[ib].y + faces[ib].height / 2;
+		}
 	}
 	
 	if (faces.size())
 	{
-		crop = frame(faces[ib]);
-
 		//resize(crop, res, Size(128, 128), 0, 0, INTER_LINEAR); // This will be needed later while saving images. res is also of type Mat. focus is on inputarray.
-		//cvtColor(crop, gray, CV_BGR2GRAY); // Convert cropped image to Grayscale
-
-		Point pt1(faces[ib].x, faces[ib].y);
-		Point pt2((faces[ib].x + faces[ib].width), (faces[ib].y + faces[ib].height));
-		rectangle(frame, pt1, pt2, Scalar(0, 255, 0), 2, 8, 0); //Coords pt1 are of left corner. 
+				
+		rectangle(frame, pt[0], pt[1], Scalar(0, 255, 0), 2, 8, 0); //Coords pt1 are of left corner. 
 
 		double buffer_width, buffer_height;
 		buffer_width = 0.1*faces[ib].width;
 		buffer_height = 0.1*faces[ib].height;
 
-		Point Bpt1(pt1.x + ((faces[ib].width - buffer_width) / 2), pt1.y + ((faces[ib].height - buffer_height) / 2));
-		Point Bpt2(pt1.x + ((faces[ib].width + buffer_width) / 2), pt1.y + ((faces[ib].height + buffer_height) / 2));
+		Point Bpt1(pt[0].x + ((faces[ib].width - buffer_width) / 2), pt[0].y + ((faces[ib].height - buffer_height) / 2));
+		Point Bpt2(pt[0].x + ((faces[ib].width + buffer_width) / 2), pt[0].y + ((faces[ib].height + buffer_height) / 2));
 		rectangle(frame, Bpt2, Bpt1, Scalar(255, 0, 0), 2, 8, 0); //Enclsoing the buffer zone. Defined to deal with slight variations in center of rectangle.
 
-		Point cen_rec(faces[ib].x + faces[ib].width / 2, faces[ib].y + faces[ib].height / 2);
-		circle(frame, cen_rec, 1, Scalar(0, 0, 255), CV_FILLED); //Marks the center of detected face.
+		crop = frame(faces[ib]);
 
+		//cvtColor(crop, gray, COLOR_BGR2GRAY);
+		//equalizeHist(gray, gray);
+		//try {
+		//	eye_cascade.detectMultiScale(gray, eyes, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(10, 10));
+		//}
+		//catch (Exception ex) {
+		//	printf("EXX");
+		//}
+		//if (eyes.size())
+		//{  
+		//	Point Ept1;
+		//	Point Ept2;
+
+		//	for (int i = 0; i < eyes.size(); ++i)
+		//	{
+		//		Ept1.x = eyes[i].x; // Ept1 is of type point while eyes[i] is of type rectangle but both the data members are of the same type  
+		//		Ept1.y = eyes[i].y;
+		//		
+		//		Ept2.x = eyes[i].x + eyes[i].width; 
+		//		Ept2.y = eyes[i].y + eyes[i].height;
+
+		//		rectangle(frame, Ept1, Ept2, Scalar(0, 255, 0), 2, 8, 0);
+		//	}	 
+		//  
+		//} 
+		
 		arrowedLine(frame, cen_win, Point(cen_win.x, cen_rec.y), Scalar(255, 0, 255), 1, 8, 0, 0.1); //Helps visualise the extent of camera tilt required to center face 
 		arrowedLine(frame, cen_win, Point(cen_rec.x, cen_win.y), Scalar(255, 0, 255), 1, 8, 0, 0.1); //Helps viualise the extent of panning required to center face.  
-
-
-	   //if (faces[ib].width > 0 && faces[ib].width < 40) cout << "Crop area: " << faces[ib].width << " * " << faces[ib].height;
-	   //We should probably exclude faces with width less than 40 pixels as face detection with them gives inconsistent results 
 
 	   // Show image
 		sstm << "Crop area size: " << faces[ib].width << "x" << faces[ib].height << " Size of window: " << frame.size().width << '*' << frame.size().height;
@@ -168,5 +218,11 @@ void CallBackFunc(int event, int x, int y, int flags, void * userdata)
 	{
 	cout << "Mouse move over the window - position (" << x << ", " << y << ")" << endl;
 
-	} */
+	}*/
 }
+
+//void Rect_Point(Rect R, Point P)
+//{
+//	 P.x = R.x
+//      
+//}
